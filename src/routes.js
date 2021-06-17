60ed96d0220e33cea8668a62e08b05b439123eb7
antonioneto00 <you@example.com>
2021-06-17 14:07:02 -0300

    Frontend com Select Details

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
  
 import MainProduto from './pages/produto/main';
+import DetalhesProduto from './pages/produto/detalhes';
  
 const Routes = () => (
  
     <BrowserRouter>
         <Switch>
             <Route exact path="/produtos" component={MainProduto} />
+            <Route path="/produtos/:id" component={DetalhesProduto} />
         </Switch>
     </BrowserRouter>
 )
